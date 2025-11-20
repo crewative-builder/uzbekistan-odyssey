17adde112d6ee373dbde867c9394b5c6556d7359
Meyrbek <saydivakasovm@gmail.com>
2025-11-20 14:50:39 +0500

    Initial setup

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/uzbekistan-odyssey/", // <--- IMPORTANT: This matches your future Repo Name
+})
